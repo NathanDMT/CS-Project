be7c0695d66eb915b627f5cddbfeaf28076df250
NathanDMT <demoineretnathan@gmail.com>
2023-11-13 12:00:41 +0100

    Modification de la page 2 & 3 avec ajouts de la fermeture du programme à tous les coups peu importe la façon de fermeture du programme

--- CS-Project/Form3.Designer.cs
+++ CS-Project/Form3.Designer.cs
@@ -28,20 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.checkBoxTout = new System.Windows.Forms.CheckBox();
+            this.checkBoxAP = new System.Windows.Forms.CheckBox();
+            this.checkBoxAE = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // menuButton
+            // 
+            this.menuButton.Location = new System.Drawing.Point(777, 462);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(85, 42);
+            this.menuButton.TabIndex = 3;
+            this.menuButton.Text = "Retour au menu";
+            this.menuButton.UseVisualStyleBackColor = true;
+            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(308, 71);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 15);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Informations de la commande sélectionnée";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(311, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(260, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Gestionnaire des commandes";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(627, 275);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(191, 13);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Documents";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(627, 150);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(191, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(627, 218);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(191, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox6.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox6.Location = new System.Drawing.Point(627, 124);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(191, 13);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "Rechercher un client";
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox7.Location = new System.Drawing.Point(627, 71);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(191, 15);
+            this.textBox7.TabIndex = 10;
+            this.textBox7.Text = "Recherche spécifique";
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox8.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox8.Location = new System.Drawing.Point(627, 192);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(191, 13);
+            this.textBox8.TabIndex = 11;
+            this.textBox8.Text = "Rechercher une commande";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(627, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Liste de colisage";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox9.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox9.Location = new System.Drawing.Point(40, 71);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(138, 15);
+            this.textBox9.TabIndex = 13;
+            this.textBox9.Text = "Liste des commandes";
+            // 
+            // checkBoxTout
+            // 
+            this.checkBoxTout.AutoSize = true;
+            this.checkBoxTout.Location = new System.Drawing.Point(40, 124);
+            this.checkBoxTout.Name = "checkBoxTout";
+            this.checkBoxTout.Size = new System.Drawing.Size(48, 17);
+            this.checkBoxTout.TabIndex = 15;
+            this.checkBoxTout.Text = "Tout";
+            this.checkBoxTout.UseVisualStyleBackColor = true;
+            this.checkBoxTout.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBoxAP
+            // 
+            this.checkBoxAP.AutoSize = true;
+            this.checkBoxAP.Location = new System.Drawing.Point(94, 124);
+            this.checkBoxAP.Name = "checkBoxAP";
+            this.checkBoxAP.Size = new System.Drawing.Size(62, 17);
+            this.checkBoxAP.TabIndex = 16;
+            this.checkBoxAP.Text = "A payer";
+            this.checkBoxAP.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAE
+            // 
+            this.checkBoxAE.AutoSize = true;
+            this.checkBoxAE.Location = new System.Drawing.Point(162, 124);
+            this.checkBoxAE.Name = "checkBoxAE";
+            this.checkBoxAE.Size = new System.Drawing.Size(76, 17);
+            this.checkBoxAE.TabIndex = 17;
+            this.checkBoxAE.Text = "A expédier";
+            this.checkBoxAE.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(874, 516);
+            this.Controls.Add(this.checkBoxAE);
+            this.Controls.Add(this.checkBoxAP);
+            this.Controls.Add(this.checkBoxTout);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuButton);
             this.Name = "Form3";
             this.Text = "Menagelec - Gestion des commandes";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button menuButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.CheckBox checkBoxTout;
+        private System.Windows.Forms.CheckBox checkBoxAP;
+        private System.Windows.Forms.CheckBox checkBoxAE;
     }
 }
